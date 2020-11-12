78e0328688ad270762f73f5bae135e14927afe9e
khyens <hoypie@gmail.com>
2020-11-13 00:36:19 +0900

    Update purchase.js

--- iphone/purchase.js
+++ iphone/purchase.js
@@ -1,7 +1,7 @@
 function purchase() {
     let timestamp = +new Date();
 
-    let url = "https://checkout.coupang.com/direct/checkout/" + timestamp + "?item[]=71834191703:1";
+    let url = "https://checkout.coupang.com/direct/checkout/" + timestamp + "?item[]=71834191703";
 
     window.open(url, '_blank');
 }
